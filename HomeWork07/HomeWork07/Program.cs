a7fe25abfc74f27df47e127b8c3a9288edca4ea3
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-24 23:06:02 +0300

    Resolved task

--- HomeWork07/HomeWork07/Program.cs
+++ HomeWork07/HomeWork07/Program.cs
@@ -1,256 +1,245 @@
-﻿bool isEnd = false;
+﻿bool isGameOver = false;
+bool isEndX = false;
+bool isEndO = false;
 string playerX = "";
 string playerO = "";
 
-string cell1 = "1";
-string cell2 = "2";
-string cell3 = "3";
-string cell4 = "4";
-string cell5 = "5";
-string cell6 = "6";
-string cell7 = "7";
-string cell8 = "8";
-string cell9 = "9";
+string c1 = "1";
+string c2 = "2";
+string c3 = "3";
+string c4 = "4";
+string c5 = "5";
+string c6 = "6";
+string c7 = "7";
+string c8 = "8";
+string c9 = "9";
 
-//Виграшні комбінації:
+//Winning combinations
 #region
 //1,2,3
-bool winCombinationX1 = cell1 == "X" && cell2 == "X" && cell3 == "X";
+bool xWin1 = c1 == "X" && c2 == "X" && c3 == "X";
 //4,5,6
-bool winCombinationX2 = cell4 == "X" && cell5 == "X" && cell6 == "X";
+bool xWin2 = c4 == "X" && c5 == "X" && c6 == "X";
 //7,8,9
-bool winCombinationX3 = cell7 == "X" && cell8 == "X" && cell9 == "X";
+bool xWin3 = c7 == "X" && c8 == "X" && c9 == "X";
 //1,4,7
-bool winCombinationX4 = cell1 == "X" && cell4 == "X" && cell7 == "X";
+bool xWin4 = c1 == "X" && c4 == "X" && c7 == "X";
 //2,5,8
-bool winCombinationX5 = cell2 == "X" && cell5 == "X" && cell8 == "X";
+bool xWin5 = c2 == "X" && c5 == "X" && c8 == "X";
 //3,6,9
-bool winCombinationX6 = cell9 == "X" && cell6 == "X" && cell3 == "X";
+bool xWin6 = c3 == "X" && c6 == "X" && c9 == "X";
 //1,5,9
-bool winCombinationX7 = cell1 == "X" && cell5 == "X" && cell9 == "X";
+bool xWin7 = c1 == "X" && c5 == "X" && c9 == "X";
 //3,5,7
-bool winCombinationX8 = cell7 == "X" && cell5 == "X" && cell3 == "X";
+bool xWin8 = c7 == "X" && c5 == "X" && c3 == "X";
 
 
 //1,2,3
-bool winCombinationO1 = cell1 == "O" && cell2 == "O" && cell3 == "O";
+bool oWin1 = c1 == "O" && c2 == "O" && c3 == "O";
 //4,5,6
-bool winCombinationO2 = cell4 == "O" && cell5 == "O" && cell6 == "O";
+bool oWin2 = c4 == "O" && c5 == "O" && c6 == "O";
 //7,8,9
-bool winCombinationO3 = cell7 == "O" && cell8 == "O" && cell9 == "O";
+bool oWin3 = c7 == "O" && c8 == "O" && c9 == "O";
 //1,4,7
-bool winCombinationO4 = cell1 == "O" && cell4 == "O" && cell7 == "O";
+bool oWin4 = c1 == "O" && c4 == "O" && c7 == "O";
 //2,5,8
-bool winCombinationO5 = cell2 == "O" && cell5 == "O" && cell8 == "O";
+bool oWin5 = c2 == "O" && c5 == "O" && c8 == "O";
 //3,6,9
-bool winCombinationO6 = cell9 == "O" && cell6 == "O" && cell3 == "O";
+bool oWin6 = c3 == "O" && c6 == "O" && c9 == "O";
 //1,5,9
-bool winCombinationO7 = cell1 == "O" && cell5 == "O" && cell9 == "O";
+bool oWin7 = c1 == "O" && c5 == "O" && c9 == "O";
 //3,5,7
-bool winCombinationO8 = cell7 == "O" && cell5 == "O" && cell3 == "O";
+bool oWin8 = c7 == "O" && c5 == "O" && c3 == "O";
 #endregion
 
-//Перше виведення
-for (int i = 1; i <= 9; i++)
-{
-	switch (i)
-	{
-		case 1:
-			Console.Write($"{cell1} | ");
-			break;
-		case 2:
-			Console.Write($"{cell2} | ");
-			break;
-		case 3:
-			Console.WriteLine($"{cell3}");
-			break;
-		case 4:
-			Console.WriteLine($"----------");
-			Console.Write($"{cell4} | ");
-			break;
-		case 5:
-			Console.Write($"{cell5} | ");
-			break;
-		case 6:
-			Console.WriteLine($"{cell6}");
-			break;
-		case 7:
-			Console.WriteLine($"----------");
-			Console.Write($"{cell7} | ");
-			break;
-		case 8:
-			Console.Write($"{cell8} | ");
-			break;
-		case 9:
-			Console.WriteLine($"{cell9}");
-			break;
-	}
-}
+//Initial table
+Console.WriteLine($"{c1} | {c2} | {c3}\n" +
+				  $"----------\n" +
+				  $"{c4} | {c5} | {c6}\n" +
+				  $"----------\n" +
+				  $"{c7} | {c8} | {c9}\n");
 
-while (!isEnd)
+while (!isGameOver)
 {
-	Console.Write($"Player 1, enter number you want to choose: ");
-	playerX = Console.ReadLine();
-
-	switch (playerX)
+	while (!isEndX)
 	{
-		case "1" when cell1 != "O":
-			cell1 = "X";
-			break;
-		case "2" when cell2 != "O":
-			cell2 = "X";
-			break;
-		case "3" when cell3 != "O":
-			cell3 = "X";
-			break;
-		case "4" when cell4 != "O":
-			cell4 = "X";
-			break;
-		case "5" when cell5 != "O":
-			cell5 = "X";
-			break;
-		case "6" when cell6 != "O":
-			cell6 = "X";
-			break;
-		case "7" when cell7 != "O":
-			cell7 = "X";
-			break;
-		case "8" when cell8 != "O":
-			cell8 = "X";
-			break;
-		case "9" when cell9 != "O":
-			cell9 = "X";
-			break;
+		Console.Write($"Player 1, choose cell (1-9): ");
+		playerX = Console.ReadLine();
+
+		switch (playerX)
+		{
+			case "1" when c1 != "O" && c1 != "X":
+				c1 = "X";
+				isEndX = true;
+				break;
+			case "2" when c2 != "O" && c2 != "X":
+				c2 = "X";
+				isEndX = true;
+				break;
+			case "3" when c3 != "O" && c3 != "X":
+				c3 = "X";
+				isEndX = true;
+				break;
+			case "4" when c4 != "O" && c4 != "X":
+				c4 = "X";
+				isEndX = true;
+				break;
+			case "5" when c5 != "O" && c5 != "X":
+				c5 = "X";
+				isEndX = true;
+				break;
+			case "6" when c6 != "O" && c6 != "X":
+				c6 = "X";
+				isEndX = true;
+				break;
+			case "7" when c7 != "O" && c7 != "X":
+				c7 = "X";
+				isEndX = true;
+				break;
+			case "8" when c8 != "O" && c8 != "X":
+				c8 = "X";
+				isEndX = true;
+				break;
+			case "9" when c9 != "O" && c9 != "X":
+				c9 = "X";
+				isEndX = true;
+				break;
+			default:
+				Console.WriteLine("Invalid input or cell is filled. Please try again.");
+				break;
+		}
 	}
+
+	//Updating the values ​​of winning combinations for the first player
 	#region
 	//1,2,3
-	winCombinationX1 = cell1 == "X" && cell2 == "X" && cell3 == "X";
+	xWin1 = c1 == "X" && c2 == "X" && c3 == "X";
 	//4,5,6
-	winCombinationX2 = cell4 == "X" && cell5 == "X" && cell6 == "X";
+	xWin2 = c4 == "X" && c5 == "X" && c6 == "X";
 	//7,8,9
-	winCombinationX3 = cell7 == "X" && cell8 == "X" && cell9 == "X";
+	xWin3 = c7 == "X" && c8 == "X" && c9 == "X";
 	//1,4,7
-	winCombinationX4 = cell1 == "X" && cell4 == "X" && cell7 == "X";
+	xWin4 = c1 == "X" && c4 == "X" && c7 == "X";
 	//2,5,8
-	winCombinationX5 = cell2 == "X" && cell5 == "X" && cell8 == "X";
+	xWin5 = c2 == "X" && c5 == "X" && c8 == "X";
 	//3,6,9
-	winCombinationX6 = cell9 == "X" && cell6 == "X" && cell3 == "X";
+	xWin6 = c3 == "X" && c6 == "X" && c9 == "X";
 	//1,5,9
-	winCombinationX7 = cell1 == "X" && cell5 == "X" && cell9 == "X";
+	xWin7 = c1 == "X" && c5 == "X" && c9 == "X";
 	//3,5,7
-	winCombinationX8 = cell7 == "X" && cell5 == "X" && cell3 == "X";
-
-	Console.WriteLine($"{cell1} | {cell2} | {cell3}\n" +
-					  $"----------\n" +
-					  $"{cell4} | {cell5} | {cell6}\n" +
-					  $"----------\n" +
-					  $"{cell7} | {cell8} | {cell9}\n");
+	xWin8 = c7 == "X" && c5 == "X" && c3 == "X";
 	#endregion
 
-	if (winCombinationX1 || winCombinationX2 || winCombinationX3 || winCombinationX4 || winCombinationX5 || winCombinationX6 || winCombinationX7 || winCombinationX8)
+	//Print updated table
+	Console.WriteLine($"{c1} | {c2} | {c3}\n" +
+					  $"----------\n" +
+					  $"{c4} | {c5} | {c6}\n" +
+					  $"----------\n" +
+					  $"{c7} | {c8} | {c9}\n");
+
+	//Check if first player won
+	if (xWin1 || xWin2 || xWin3 || xWin4 || xWin5 || xWin6 || xWin7 || xWin8)
 	{
 		Console.WriteLine("Player 1 win");
-		isEnd = true;
+		isGameOver = true;
 	}
 
-	Console.Write($"Player 2, enter number you want to choose: ");
-	playerO = Console.ReadLine();
+	isEndX = false;
 
-	switch (playerO)
+	while (!isEndO && !isGameOver)
 	{
-		case "1" when cell1 != "X":
-			cell1 = "O";
-			break;
-		case "2" when cell2 != "X":
-			cell2 = "O";
-			break;
-		case "3" when cell3 != "X":
-			cell3 = "O";
-			break;
-		case "4" when cell4 != "X":
-			cell4 = "O";
-			break;
-		case "5" when cell5 != "X":
-			cell5 = "O";
-			break;
-		case "6" when cell6 != "X":
-			cell6 = "O";
-			break;
-		case "7" when cell7 != "X":
-			cell7 = "O";
-			break;
-		case "8" when cell8 != "X":
-			cell8 = "O";
-			break;
-		case "9" when cell9 != "X":
-			cell9 = "O";
-			break;
+		Console.Write($"Player 2, choose cell (1-9): ");
+		playerO = Console.ReadLine();
+
+		switch (playerO)
+		{
+			case "1" when c1 != "X" && c1 != "O":
+				c1 = "O";
+				isEndO = true;
+				break;
+			case "2" when c2 != "X" && c2 != "O":
+				c2 = "O";
+				isEndO = true;
+				break;
+			case "3" when c3 != "X" && c3 != "O":
+				c3 = "O";
+				isEndO = true;
+				break;
+			case "4" when c4 != "X" && c4 != "O":
+				c4 = "O";
+				isEndO = true;
+				break;
+			case "5" when c5 != "X" && c5 != "O":
+				c5 = "O";
+				isEndO = true;
+				break;
+			case "6" when c6 != "X" && c6 != "O":
+				c6 = "O";
+				isEndO = true;
+				break;
+			case "7" when c7 != "X" && c7 != "O":
+				c7 = "O";
+				isEndO = true;
+				break;
+			case "8" when c8 != "X" && c8 != "O":
+				c8 = "O";
+				isEndO = true;
+				break;
+			case "9" when c9 != "X" && c9 != "O":
+				c9 = "O";
+				isEndO = true;
+				break;
+			default:
+				Console.WriteLine("All cells are filled");
+				isEndO = true;
+				break;
+		}
 	}
 
+	//Updating the values ​​of winning combinations for the second player
 	#region
 	//1,2,3
-	winCombinationO1 = cell1 == "O" && cell2 == "O" && cell3 == "O";
+	oWin1 = c1 == "O" && c2 == "O" && c3 == "O";
 	//4,5,6
-	winCombinationO2 = cell4 == "O" && cell5 == "O" && cell6 == "O";
+	oWin2 = c4 == "O" && c5 == "O" && c6 == "O";
 	//7,8,9
-	winCombinationO3 = cell7 == "O" && cell8 == "O" && cell9 == "O";
+	oWin3 = c7 == "O" && c8 == "O" && c9 == "O";
 	//1,4,7
-	winCombinationO4 = cell1 == "O" && cell4 == "O" && cell7 == "O";
+	oWin4 = c1 == "O" && c4 == "O" && c7 == "O";
 	//2,5,8
-	winCombinationO5 = cell2 == "O" && cell5 == "O" && cell8 == "O";
+	oWin5 = c2 == "O" && c5 == "O" && c8 == "O";
 	//3,6,9
-	winCombinationO6 = cell9 == "O" && cell6 == "O" && cell3 == "O";
+	oWin6 = c3 == "O" && c6 == "O" && c9 == "O";
 	//1,5,9
-	winCombinationO7 = cell1 == "O" && cell5 == "O" && cell9 == "O";
+	oWin7 = c1 == "O" && c5 == "O" && c9 == "O";
 	//3,5,7
-	winCombinationO8 = cell7 == "O" && cell5 == "O" && cell3 == "O";
-
-	Console.WriteLine($"{cell1} | {cell2} | {cell3}\n" +
-					  $"----------\n" +
-					  $"{cell4} | {cell5} | {cell6}\n" +
-					  $"----------\n" +
-					  $"{cell7} | {cell8} | {cell9}\n");
+	oWin8 = c7 == "O" && c5 == "O" && c3 == "O";
 	#endregion
 
-	if (winCombinationO1 || winCombinationO2 || winCombinationO3 || winCombinationO4 || winCombinationO5 || winCombinationO6 || winCombinationO7 || winCombinationO8)
+	//Print updated table
+	Console.WriteLine($"{c1} | {c2} | {c3}\n" +
+					  $"----------\n" +
+					  $"{c4} | {c5} | {c6}\n" +
+					  $"----------\n" +
+					  $"{c7} | {c8} | {c9}\n");
+
+	//Check if second player won
+	if (oWin1 || oWin2 || oWin3 || oWin4 || oWin5 || oWin6 || oWin7 || oWin8)
 	{
 		Console.WriteLine("Player 2 win");
-		isEnd = true;
+		isGameOver = true;
 	}
-}
 
-//for (int i = 1; i <= 9; i++)
-//{
-//	switch (i)
-//	{
-//		case 1:
-//			Console.Write($"{cell1} | ");
-//			break;
-//		case 2:
-//			Console.Write($"{cell2} | ");
-//			break;
-//		case 3:
-//			Console.WriteLine($"{cell3}");
-//			break;
-//		case 4:
-//			Console.WriteLine($"----------");
-//			Console.Write($"{cell4} | ");
-//			break;
-//		case 5:
-//			Console.Write($"{cell5} | ");
-//			break;
-//		case 6:
-//			Console.WriteLine($"{cell6}");
-//			break;
-//		case 7:
-//			Console.WriteLine($"----------");
-//			Console.Write($"{cell7} | ");
-//			break;
-//		case 8:
-//			Console.Write($"{cell8} | ");
-//			break;
-//		case 9:
-//			Console.WriteLine($"{cell9}");
-//			break;
-//	}
-//}
+	//Check if it's draw
+	if (!xWin1 && !xWin2 && !xWin3 && !xWin4 && !xWin5 && !xWin6 && !xWin7 && !xWin8
+		&& !oWin1 && !oWin2 && !oWin3 && !oWin4 && !oWin5 && !oWin6 && !oWin7 && !oWin8
+		&& c1 != "1" && c2 != "2" && c3 != "3"
+		&& c4 != "4" && c5 != "5" && c6 != "6"
+		&& c7 != "7" && c8 != "8" && c9 != "9")
+	{
+		Console.WriteLine("It`s draw!");
+		isGameOver = true;
+	}
+
+	isEndO = false;
+}
